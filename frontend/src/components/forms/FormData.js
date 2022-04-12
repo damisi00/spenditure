f4ca4dc551070f4f9a5049f92bd1a25add699ea6
adeoyewole028 <adeoyewole028@gmail.com>
2022-04-12 20:55:41 +0100

    working on formDate

---
+++ frontend/src/components/forms/FormData.js
@@ -0,0 +1,23 @@
+export const FormData = [
+  {
+    label: "Full Name",
+    id: "name",
+    type: "text",
+    placeholder: "Bianca Grace",
+  },
+  {
+    label: "Mobile Phone",
+    type: "tel",
+    placeholder: "+234800000000",
+  },
+  {
+    label: "Email address",
+    type: "email",
+    placeholder: "name@domain.com",
+  },
+  {
+    label: "Password",
+    type: "password",
+    placeholder: "************",
+  },
+];
