30b0b787e350a1c92b0e59394434a927d5047db4
adeoyewole028 <adeoyewole028@gmail.com>
2022-04-30 21:27:34 +0100

    removed card class

--- frontend/src/components/about/AboutUs.jsx
+++ frontend/src/components/about/AboutUs.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const AboutUs = ({ title, desc }) => {
   return (
-    <div className="col-sm-4 col-md-4 col-lg-4 mt-5 px-5">
-      <div className="card border-0 h-100">
+    <div className="col d-flex justify-content-center">
+      <div>
         <h2 className="card-title">{title}</h2>
         <div className="about--believe card-body  text-start shadow-lg">
           <p className="card-text">{desc}</p>
