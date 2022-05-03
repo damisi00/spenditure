ca2112a5e6e844c53a2d1be42894db64b51d842e
adeoyewole028 <adeoyewole028@gmail.com>
2022-05-03 18:36:55 +0100

    removed dots

--- frontend/src/components/home/LeftNav.js
+++ frontend/src/components/home/LeftNav.js
@@ -6,39 +6,6 @@ const LeftNav = () => {
   return (
     <div className="">
       <div className="">
-        <div
-          className="ms-2"
-          id="dots"
-          style={{ display: "flex", padding: "20px" }}
-        >
-          <div
-            style={{
-              borderRadius: "50%",
-              backgroundColor: "#3ec3d5",
-              width: "16px",
-              height: "16px",
-              margin: "0px",
-            }}
-          ></div>
-          <div
-            style={{
-              borderRadius: "50%",
-              backgroundColor: "#ff5460",
-              width: "16px",
-              height: "16px",
-              margin: "0px 10px",
-            }}
-          ></div>
-          <div
-            style={{
-              borderRadius: "50%",
-              backgroundColor: "#41dc65",
-              width: "16px",
-              height: "16px",
-              margin: "0px",
-            }}
-          ></div>
-        </div>
 
         <div id="logo" className="mb-3 me-3">
           <Link to="/">
