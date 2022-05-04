4171b8a1686f4d654d80ffd1a0efb293bd488331
adeoyewole028 <adeoyewole028@gmail.com>
2022-05-04 10:07:10 +0100

    fixed home component

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,27 +1,22 @@
 import React from "react";
 import RightNav from "../home/RightNav";
 import LeftNav from "../home/LeftNav";
-import Main from "../home/Main"
+import Main from "../home/Main";
 const Home = () => {
   return (
-    <main>
-      <section className="">
-        <div className="row">
-          
-          <div className="left-sidebar col-lg-3 col-md-2 d-none d-md-none d-lg-block">
-            <LeftNav />
-          </div>
+    <div className="home-main row">
+      <div className="left-sidebar col-lg-3 col-md-2 d-none d-md-none d-lg-block">
+        <LeftNav />
+      </div>
 
-          <div id="main" className="col-md-8 ">
-            <Main />
-          </div>
+      <div id="main" className="col-md-8 col-lg-6">
+        <Main />
+      </div>
 
-          <div className="right-sidebar col-md-1 d-none d-md-none d-lg-block">
-            <RightNav />
-          </div>
-        </div>
-      </section>
-    </main>
+      <div className="right-sidebar col-lg-2 col-md-1 d-none d-md-none d-lg-block">
+        <RightNav />
+      </div>
+    </div>
   );
 };
 
